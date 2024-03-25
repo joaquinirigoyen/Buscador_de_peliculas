6a7fcd0c08d8554fa8e1a0cabd0d8a5f641b6c5a
joaquinirigoyen <joaquin.iri25@gmail.com>
2024-03-25 18:02:08 -0300

    subiendo todo

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BuscadorPeliculas } from "./BuscadorPeliculas";
+import "./styles/styles.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BuscadorPeliculas />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
